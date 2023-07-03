2650078aab1b30566155205de0155acf5ceaa74e
renlin-code <renelj1997@gmail.com>
2023-07-03 15:14:00 +0500

    Added vue-router and history mode in order to fix Netlify 404 error

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import { createHead } from '@vueuse/head'
 import App from './App.vue';
 import './assets/global.scss';
+import router from './router'
 import i18n from './locales/i18n';
 import rem from './plugins/rem';
 
 const app = createApp(App);
-const head = createHead()
+const head = createHead();
 
 app.use(head);
+app.use(router);
 app.use(i18n);
 app.use(rem);
 app.mount('#app');
